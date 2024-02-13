ae87f8937eeda5a03d099aec7b670501cc528155
Ezana Mulugeta <157648296+KingEzu@users.noreply.github.com>
2024-02-13 22:21:03 +0300

    Rent.jsx

--- src/routes/Rent.jsx
+++ src/routes/Rent.jsx
@@ -1,9 +1,15 @@
-
+import RentH from '../componets/rentDe/RentH'
+import Navbar from '../componets/Navbar/Navbar';
+import React from 'react';
 
 function  Rent (){
     return(
-        <></>
-
+        <>
+            <Navbar/>
+            <RentH/>
+        
+        </>
+       
       
     );
 }
